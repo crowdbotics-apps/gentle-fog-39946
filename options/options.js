706ae6dca5048357a5773d924e410534515414c1
Crowdbotics <team@crowdbotics.com>
2023-04-14 19:44:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-fog-39946",
+  url: "https://gentle-fog-39946.botics.co",
+  api: "https://gentle-fog-39946.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
